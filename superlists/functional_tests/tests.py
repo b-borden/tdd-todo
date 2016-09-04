dd201570dc20539d7fa027b8cc924532c938e5a4
Brandon Borden <bborden@ualberta.ca>
2016-09-04 16:01:52 -0700

    new URL + view for adding to existing lists.  FT passing

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -84,11 +84,3 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
-
-        self.fail('Finish the test!')
-
-
-        # She visits that URL - her to-do list is still there
-
-        # Satisfied, she goes back to sleep
