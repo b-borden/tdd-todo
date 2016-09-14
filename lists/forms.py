4e40496d28190d2d7dcd663b261c6d2b47dd0acd
Brandon Borden <bborden@ualberta.ca>
2016-09-13 21:46:06 -0700

    Last commit did not actually enforce unique items.  This one does

--- lists/forms.py
+++ lists/forms.py
@@ -1,5 +1,5 @@
-from django.core.exceptions import ValidationError
 from django import forms
+from django.core.exceptions import ValidationError
 
 from lists.models import Item
 
@@ -7,10 +7,6 @@ EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
-
-    def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
 
     class Meta:
         model = Item
@@ -25,10 +21,18 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_ITEM_ERROR}
         }
 
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
+
 class ExistingListItemForm(ItemForm):
+
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.instance.list = for_list
+
 
     def validate_unique(self):
         try:
@@ -36,3 +40,9 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
+
